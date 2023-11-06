80b4a7ec800a60ec5de5c569dcb671e5a90f34be
kaprim4 <87yahya@gmail.com>
2023-11-06 01:08:25 +0100

    Add GasStation JPA

--- src/main/java/com/winxo/PortailEnelpWs/config/SecurityConfiguration.java
+++ src/main/java/com/winxo/PortailEnelpWs/config/SecurityConfiguration.java
@@ -31,7 +31,7 @@ public class SecurityConfiguration
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request ->
-                        request.requestMatchers("/api/v1/auth/**")
+                        request.requestMatchers("/api/v1/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
--- src/main/java/com/winxo/PortailEnelpWs/controller/GasStationController.java
+++ src/main/java/com/winxo/PortailEnelpWs/controller/GasStationController.java
@@ -12,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/gas_stations")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class GasStationController {
 
     private final GasStationService gasStationService;
