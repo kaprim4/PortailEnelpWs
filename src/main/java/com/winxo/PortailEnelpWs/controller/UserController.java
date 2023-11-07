312564d0d6f330f84d0470bbdc0b6af98aa801d2
kaprim4 <87yahya@gmail.com>
2023-11-07 19:10:16 +0100

    Add GasStation and User JPA

--- src/main/java/com/winxo/PortailEnelpWs/controller/UserController.java
+++ src/main/java/com/winxo/PortailEnelpWs/controller/UserController.java
@@ -12,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
     private final UserService userService;
